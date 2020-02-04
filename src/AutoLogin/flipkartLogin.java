4305b75adab4cedc339e53c5d1b7f5c5bd72ff19
Anit Singh <anit.singh@sourcefuse.com>
2020-02-05 00:13:32 +0530

    Change

--- src/AutoLogin/flipkartLogin.java
+++ src/AutoLogin/flipkartLogin.java
@@ -14,7 +14,7 @@ public class flipkartLogin {
 		driver.get("https://www.flipkart.com/");
 		
 		String username = "8427615969";
-		String password = "Anit08Oct98";
+		String password = "**********";
 		
 		driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/input[1]")).sendKeys(username);
 		driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[2]/input[1]")).sendKeys(password);
